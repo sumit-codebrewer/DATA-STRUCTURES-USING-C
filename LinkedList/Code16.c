1b2145227f406de8593e3e2bdd21d59106a800fc
Sumit Chutani <71552669+sumit-codebrewer@users.noreply.github.com>
2020-11-24 04:38:10 +0530

    Added insertion in singly circular linkedlist
    Menu Driven Program for insertion at different positions in a Singly Circular Linkedlist

---
+++ LinkedList/Code16.c
@@ -0,0 +1,175 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+                            //Insertion and Display in Singly Circular LinkedList
+#include <stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+}*head=NULL;
+void create()
+{
+    int ch,val;
+    do
+    {
+        struct node *NN,*current;
+        NN=(struct node *)malloc(sizeof(struct node));
+        printf("\nEnter data:");
+        scanf("%d",&val);
+        NN->data=val;
+        NN->next=NULL;
+        if(head==NULL)
+        {
+            head=NN;
+            current=NN;
+            head->next=head;
+        }
+        else
+        {
+            current->next=NN;
+            current=NN;
+            current->next=head;
+        }
+        printf("\nDo you want to continue?(1 for Yes 0 for No):");
+        scanf("%d",&ch);
+    }while(ch!=0&&ch==1);
+}
+void insert_beg()
+{
+    struct node *NN,*temp;
+    int val;
+    NN=(struct node *)malloc(sizeof(struct node));
+    printf("\nEnter the value you want to insert:");
+    scanf("%d",&val);
+    NN->data=val;
+    NN->next=NULL;
+    if(head==NULL)
+    {
+        head=NN;
+        head->next=head;
+    }
+    else
+    {
+        temp=head;
+        while(temp->next!=head)
+        {
+            temp=temp->next;
+        }
+        temp->next=NN;
+        NN->next=head;
+        head=NN;
+    }
+}
+void insert_mid()
+{
+    struct node *NN,*temp1,*temp2;
+    int val,position;
+    NN=(struct node *)malloc(sizeof(struct node));
+    printf("\nEnter the value you want to insert:");
+    scanf("%d",&val);
+    NN->data=val;
+    NN->next=NULL;
+    if(head==NULL)
+    {
+        head=NN;
+        head->next=head;
+    }
+    else
+    {
+        temp1=head;
+        temp2=temp1->next;
+        printf("\nEnter the position at which you want to insert:");
+        scanf("%d",&position);
+        for(int i=0;i<position-2;i++)
+        {
+            temp1=temp1->next;
+            temp2=temp2->next;
+        }
+        temp1->next=NN;
+        NN->next=temp2;
+    }
+}
+void insert_end()
+{
+    struct node *NN,*temp;
+    int val,position;
+    NN=(struct node *)malloc(sizeof(struct node));
+    printf("\nEnter the value you want to insert:");
+    scanf("%d",&val);
+    NN->data=val;
+    NN->next=NULL;
+    if(head==NULL)
+    {
+        head=NN;
+        head->next=head;
+    }
+    else
+    {
+        temp=head;
+        while(temp->next!=head)
+        {
+            temp=temp->next;
+        }
+        temp->next=NN;
+        NN->next=head;
+    }
+}
+void display()
+{
+    struct node *temp;
+    if(head==NULL)
+    {
+        printf("\nLinkedList is Empty!!!");
+    }
+    else
+    {
+        temp=head;
+        while(temp->next!=head)
+        {
+            printf("%d ",temp->data);
+            temp=temp->next;
+        }
+        printf("%d",temp->data);
+    }
+}
+int main()
+{
+   int ch;
+   do
+   {
+       printf("\n1.Create");
+       printf("\n2.Insert at beginning");
+       printf("\n3.Insert at middle");
+       printf("\n4.Insert at end");
+       printf("\n5.Display");
+       printf("\n6.Exit");
+       
+       printf("\nEnter choice:");
+       scanf("%d",&ch);
+       switch(ch)
+       {
+           case 1:create();
+                  break;
+           case 2:insert_beg();
+                  break;
+           case 3:insert_mid();
+                  break;
+            case 4:insert_end();
+                   break;
+            case 5:display();
+                   break;
+            case 6:break;
+           default:printf("\nWrong choice!!!");
+       }
+   }while(ch!=6);
+
+    return 0;
+}
+
